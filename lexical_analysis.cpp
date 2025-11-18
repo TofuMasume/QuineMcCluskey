0e7b99d7771eec3319297e519f30ba02259848a2
Tofu Masume <tofu.masume@gmail.com>
2025-11-18 22:12:17 +0900

    add: 字句解析

--- lexical_analysis.cpp
+++ lexical_analysis.cpp
@@ -7,6 +7,12 @@
 using namespace std;
 
 
+int INT_A = static_cast<char>('A');
+int INT_Z = static_cast<char>('Z');
+int INT_NOT = static_cast<char>('^');
+int INT_SEPARATOR = static_cast<char>('+');
+
+
 // 生文字列セット
 void LA::set(string rawFormula){
     rawString = rawFormula;
@@ -20,12 +26,54 @@ string LA::getRawString(){
 
 // 生文字列分割
 void LA::separateRawString(){
-    for(int index = 0; index <= rawString.size() - 1; index++){
-        separatedString[index] = rawString[index];
+    separatedString.clear();
+    separatedString.reserve(rawString.size());
+
+    for(char ch : rawString){
+        separatedString.emplace_back(1, ch);
     }
 }
 
 // 分割文字列ゲット
 vector<string> LA::getSeparatedString(){
     return separatedString;
+}
+
+// 型推定
+LA::Token LA::inferenceCharType(string str){
+    Token token{};
+    token.op = str;
+    token.type = op_type::UNKNOWN;
+
+    if(str.empty()){
+        token.type = op_type::EMPTY;
+        return token;
+    }
+
+    int toIntStr = static_cast<int>(str[0]);
+    if(INT_A <= toIntStr && toIntStr <= INT_Z){
+        token.type = op_type::CHAR;
+        return token;
+    } else if(INT_NOT == toIntStr){
+        token.type = op_type::NOT;
+    } else if(INT_SEPARATOR == toIntStr){
+        token.type = op_type::SEPARATE;
+    }
+
+    return token;
+}
+
+// 全体型推定
+void LA::lex(){
+    tokenList.clear();
+    tokenList.reserve(separatedString.size());
+
+    for(const string& elem : separatedString){
+        tokenList.push_back(inferenceCharType(elem));
+    }
+}
+
+// 字句解析済みリストゲット
+vector<LA::Token> LA::getTokenList(){
+    return tokenList;
 }
--- lexical_analysis.hpp
+++ lexical_analysis.hpp
@@ -7,32 +7,37 @@ using namespace std;
 
 class LexicalAnalysis{
 
-    enum class op_type{
-        CHAR,
-        NOT,
-        SEPARATE,
-        UNKNOWN,
-        EMPTY
-    };
 
-    struct Token{
-        string op;
-        op_type type;
-    };
 
     public:
+        enum class op_type{
+            CHAR,
+            NOT,
+            SEPARATE,
+            UNKNOWN,
+            EMPTY
+        };
+
+        typedef struct Token{
+            string op;
+            op_type type;
+        } Token;
         // funcs
-        void set(string rawFormula);
-        string getRawString();
-        vector<string> getSeparatedString();
-        void separateRawString();
+        void set(string rawFormula); // 生文字列セット
+        string getRawString(); // 生文字列ゲット
+        vector<string> getSeparatedString(); // 分割済み文字列ゲット
+        void separateRawString(); // 文字列分割
+        Token inferenceCharType(string str); // 一文字型推定
+        void lex(); // 字句解析
+        vector<Token> getTokenList(); // 字句解析済みリストゲット
 
     private:
         // funcs
         void separate(string rawFormula);
         // variables
         string rawString;
-        vector<string> separatedString = vector<string>(256, "");
+        vector<string> separatedString;
+        vector<Token> tokenList;
 };
 
 #endif
